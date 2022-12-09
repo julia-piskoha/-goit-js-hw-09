e08604e8b26d04969dadf07065acf4a0d3bb5555
julia-piskoha <109195846+julia-piskoha@users.noreply.github.com>
2022-12-09 14:20:07 +0000

    Deploying to gh-pages from @ julia-piskoha/goit-js-hw-09@c65b9a358001fcd10b9e9f3ee05e155b44223603 🚀

---
+++ 01-color-switcher.0fdadafa.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(e){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0fdadafa.js.map
---
+++ 01-color-switcher.ebdec991.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;e.addEventListener("click",(t=>{a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.ebdec991.js.map
